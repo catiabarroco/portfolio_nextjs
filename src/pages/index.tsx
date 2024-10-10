dae835582b99cdbb7614b2015579ecca65671bcc
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2024-10-10 15:30:32 +0100

    homepage style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,10 @@ import handCoding from '../../public/assets/icons/handCoding.svg';
 import { socialMedia, workExperience } from '@constants';
 import Head from 'next/head';
 import WorkCard from '@components/WorkCard';
+import { NavLink } from '@components/NavLink';
+import URLS from '@utils/PathURL';
+import { SpriteIcon } from '@components/common/SpriteIcon';
+import { SpritesCategories } from '@enums/SpritesCategories';
 
 export default function Home() {
   return (
@@ -29,16 +33,17 @@ export default function Home() {
             <Image src={handCoding} alt="icon" width={500} height={500} />
           </div>
 
-          <div className="bg-brand-secondary-200 dark:bg-brand-secondary-700 p-6 md:p-0 rounded rounded-lg md:rounded-none md:-mx-32 flex flex-col-reverse lg:flex-row gap-10 lg:gap-0">
+          <div className=" md:p-6 rounded md:rounded-none md:-mx-32 flex flex-col-reverse lg:flex-row gap-10 lg:gap-0">
             <Image
               src="/assets/images/perfilPicture.jpeg"
               alt="perfil image"
               width={640}
               height={640}
-              className="rounded rounded-lg lg:rounded-none mx-auto"
+              className="rounded mx-auto"
             />
-            <div className="md:px-20 flex flex-col justify-center gap-10 md:gap-7">
-              <div className="flex flex-col gap-4 dark:text-brand-secondary-200">
+            <div className="md:px-10 flex flex-col justify-center gap-10 md:gap-7">
+            <NavLink href={URLS.about()} className='transform hover:bg-brand-secondary-200 dark:hover:bg-brand-secondary-700  rounded-xl'>
+              <div className="flex flex-col gap-4 dark:text-brand-secondary-200 md:border-l-4 md:p-6">
                 <h2 className="text-2xl tracking-tight text-brand-secondary-800 dark:text-brand-secondary-200 sm:text-3xl font-light">
                   About me
                 </h2>
@@ -54,7 +59,19 @@ export default function Home() {
                   passages, and more recently with desktop publishing software
                   like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
+                <p className="text-brand-primary items-center flex gap-2">
+                    <span>Read more about me</span>
+                    <SpriteIcon
+                      category={SpritesCategories.CHEVRON}
+                      name="right"
+                      size="sm"
+                      title="menu"
+                    />
+                  </p>
               </div>
+
+              </NavLink>
+             
               <div className="flex gap-4">
                 {socialMedia.map(({ href, icon, iconTitle }) => {
                   return (
